59a16afa36fc1875ed943c2ddb07c8b5a7a9f66f
wanghaipo <wanghaipo@WANGHAIPO>
2019-02-13 19:59:35 +0800

    暂存

---
+++ DesktopHelper/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace DesktopHelper
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FileSystemWatcher watcher = new FileSystemWatcher();
+        }
+    }
+}
